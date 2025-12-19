a1b4ac26b797cb7cc311d29180569f213c54822c
GitHub Action <action@github.com>
2025-12-19 08:32:58 +0000

    Update mmcp to version v0.0.1

--- Formula/mmcp.rb
+++ Formula/mmcp.rb
@@ -1,40 +1,30 @@
 class Mmcp < Formula
   desc "Mambu MCP Server"
   homepage "https://www.mambu.com"
-  version "0.0.1"
-  
+  version "v0.0.1"
+
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/mambu-gmbh/mmcp/releases/download/v#{version}/mmcp-darwin-arm64.tar.gz"
-      sha256 "a38d08907de2ef7a7449820712d71491e272a52449bfb8778652f0febd5390c2"
+      url "https://github.com/mambu-gmbh/mmcp-brew/releases/download/vv0.0.1/mmcp-v0.0.1-macos-arm64.tar.gz"
+      sha256 "59254640cb4bef50b455cf57ee86d73c29bada9067be13caf08b482ac8b23c6a"
     end
   end
 
-=begin
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/mambu-gmbh/mmcp/releases/download/v#{version}/mmcp-linux-amd64.tar.gz"
-      sha256 "REPLACE_WITH_LINUX_AMD64_SHA256"
+      url "https://github.com/mambu-gmbh/mmcp-brew/releases/download/vv0.0.1/mmcp-v0.0.1-linux-amd64.tar.gz"
+      sha256 "73dac9bb520743e59d1950b60be251ecd8d5d569c91857f3abec55e2689b563c"
     elsif Hardware::CPU.arm?
-      url "https://github.com/mambu-gmbh/mmcp/releases/download/v#{version}/mmcp-linux-arm64.tar.gz"
-      sha256 "REPLACE_WITH_LINUX_ARM64_SHA256"
+      url "https://github.com/mambu-gmbh/mmcp-brew/releases/download/vv0.0.1/mmcp-v0.0.1-linux-arm64.tar.gz"
+      sha256 "a46c92805c63e0d66c9d900e0164ce06f4900b5ce96d44921d1fcbe69a7622b7"
     end
   end
-=end
 
   def install
     bin.install "mmcp"
     
-    # Remove macOS quarantine attribute to prevent "unidentified developer" warnings
     if OS.mac?
       system "xattr", "-d", "com.apple.quarantine", "#{bin}/mmcp"
     end
   end
-
-=begin
-  test do
-    system "#{bin}/mmcp", "--version"
-  end
-=end
-
 end

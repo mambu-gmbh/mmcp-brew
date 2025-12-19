184991e47f3096c7f3bbf811e719eff9811db36d
R Billeci <rbilleci@users.noreply.github.com>
2025-12-19 11:13:04 +0100

    Fix SHA256 checksums in mmcp.rb
    Updated SHA256 checksums for macOS and Linux URLs.

--- Formula/mmcp.rb
+++ Formula/mmcp.rb
@@ -5,17 +5,17 @@ class Mmcp < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/mambu-gmbh/mmcp-brew/releases/download/vv0.0.1/mmcp-v0.0.1-macos-arm64.tar.gz"
-      sha256 "59254640cb4bef50b455cf57ee86d73c29bada9067be13caf08b482ac8b23c6a"
+      url "https://github.com/mambu-gmbh/mmcp-brew/releases/download/v0.0.1/mmcp-v0.0.1-macos-arm64.tar.gz"
+      sha256 "98f47a2536f357f5869547b1b71cb9cf6140a9f2bff0c9d45900fb3b9eea78e1"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/mambu-gmbh/mmcp-brew/releases/download/vv0.0.1/mmcp-v0.0.1-linux-amd64.tar.gz"
+      url "https://github.com/mambu-gmbh/mmcp-brew/releases/download/v0.0.1/mmcp-v0.0.1-linux-amd64.tar.gz"
       sha256 "73dac9bb520743e59d1950b60be251ecd8d5d569c91857f3abec55e2689b563c"
     elsif Hardware::CPU.arm?
-      url "https://github.com/mambu-gmbh/mmcp-brew/releases/download/vv0.0.1/mmcp-v0.0.1-linux-arm64.tar.gz"
+      url "https://github.com/mambu-gmbh/mmcp-brew/releases/download/v0.0.1/mmcp-v0.0.1-linux-arm64.tar.gz"
       sha256 "a46c92805c63e0d66c9d900e0164ce06f4900b5ce96d44921d1fcbe69a7622b7"
     end
   end

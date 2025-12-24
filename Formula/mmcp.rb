b03ec26400bd677b27f7b63ee38d4d7847c34bdb
GitLab CI <ci@gitlab.com>
2025-12-24 10:05:23 +0000

    Update mmcp to version v0.0.11

--- Formula/mmcp.rb
+++ Formula/mmcp.rb
@@ -1,22 +1,22 @@
 class Mmcp < Formula
   desc "Mambu MCP Server"
   homepage "https://www.mambu.com"
-  version "v0.0.10"
+  version "v0.0.11"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/mambu-gmbh/mmcp-brew/releases/download/v0.0.10/mmcp-v0.0.10-macos-arm64.tar.gz"
-      sha256 "8f3f8a532d54c81ccfce033df87897759f3373765db85cfcd4649aaaa4f922eb"
+      url "https://github.com/mambu-gmbh/mmcp-brew/releases/download/v0.0.11/mmcp-v0.0.11-macos-arm64.tar.gz"
+      sha256 "e94f49b859989416ffff49355f3de45c77c5ad08f995f1fa5ff27d725c08b284"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/mambu-gmbh/mmcp-brew/releases/download/v0.0.10/mmcp-v0.0.10-linux-amd64.tar.gz"
-      sha256 "55446080a5cad4f5dc28dc0d3c1022088166ea2def226c2798972642ee914c14"
+      url "https://github.com/mambu-gmbh/mmcp-brew/releases/download/v0.0.11/mmcp-v0.0.11-linux-amd64.tar.gz"
+      sha256 "2a0206e2c9ca8569d633211abac213f9a91bf6216590fdd8b51bcf76a6f28a97"
     elsif Hardware::CPU.arm?
-      url "https://github.com/mambu-gmbh/mmcp-brew/releases/download/v0.0.10/mmcp-v0.0.10-linux-arm64.tar.gz"
-      sha256 "fe5cab42a0a7664ce11e29660b2f2b3a70ae2c6ad75dab5a6634ac3f684d589f"
+      url "https://github.com/mambu-gmbh/mmcp-brew/releases/download/v0.0.11/mmcp-v0.0.11-linux-arm64.tar.gz"
+      sha256 "3a5d110b8ab0f6c8703c59a1de8119c394fb521e1d087462545434ea963472eb"
     end
   end
 
